f99d1a2c33feb5b3fd5b3f5dddf57bb060f4271d
Brian Frisch <brian.frisch@gmail.com>
2016-11-29 22:06:54 +0100

    added a little bit of commenting to all that's going on inside index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// TODO: sorting
+// TODO: search indexing
+// TODO:
+// write a compiler that can read configuration, meta-data and content and compile through various renderers:
+//  - one that can write html
+//  - one that can write json index files for each node
+
+// TODO: compile template
+
 const readConfig = require('./lib/readConfig');
 const readTemplate = require('./lib/readTemplate');
 const prepareContent = require('./lib/prepareContent');
@@ -9,30 +18,24 @@ module.exports = (opts, folder) => {
 		isEditor : false // TODO: future feature - by setting this to true we can inject something into the templating, where we'll be able to do wysiwyg stuff
 	}, opts);
 
+	// let's just get our config file for this leaf in the file structure
 	let configuring = readConfig( folder );
-	let templating = configuring.then( ( config ) => {
+
+	// then let's get a hold of its template
+	let templating = configuring.then( config => {
 		return readTemplate( opts, config );
 	} );
-	let contentPreparing = templating.then( (template) => {
+
+	// then let's prepare the content
+	let contentPreparing = templating.then( template => {
 		return prepareContent( folder, template );
 	} );
 
-
-  // TODO: dataing / contentPreparing
-    // TODO: permalinking
-  // TODO: sorting
-  // TODO: search indexing
-  // TODO:
-  // write a compiler that can read configuration, meta-data and content and compile through various renderers:
-  //  - one that can write html
-  //  - one that can write json index files for each node
-
-  // TODO: compile template
-
-	return Promise.all( [ configuring, templating, contentPreparing ] ).then(values => {
+	// and let's stitch everything together
+	return Promise.all( [ configuring, templating, contentPreparing ] ).then( values => {
 		//console.log(values[0]); // config
 		//console.log(values[1]); // template
-		console.log(values[2]);
+		//console.log(values[2]); // content
 	}, reason => {
 		console.log(reason)
 	});
